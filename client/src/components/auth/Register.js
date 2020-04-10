54c61ba5b4d607bb04d8b715e57e46864305afe4
urjit2498 <urjit64@gmail.com>
2020-04-10 19:03:31 +0530

    section 7 complete

---
+++ client/src/components/auth/Register.js
@@ -0,0 +1,117 @@
+import React, { useState } from 'react';
+import { connect } from 'react-redux';
+import { Link } from "react-router-dom";
+import { setAlert } from '../../actions/alert';
+import PropTypes from "prop-types";
+
+// import axios from 'axios';
+
+const Register = ({ setAlert }) => {
+    const [ formData, setFormData ] = useState({
+        name:'',
+        email:'',
+        password:'',
+        password2:''
+    });
+
+    const { name, email, password, password2 } = formData;
+
+    const onChange = (e) =>
+      setFormData({ ...formData, [e.target.name]: e.target.value });
+
+    const onSubmit = async e => {
+        e.preventDefault();
+        if(password !== password2){
+            setAlert('password not match!', 'danger');
+        }else{
+            console.log('Success');
+
+            // const newUser = {
+            //     name,
+            //     email,
+            //     password,
+            //     password2
+            // }
+
+            // try {
+            //     const config = {
+            //         headers: {
+            //             'content-type': 'application/json'
+            //         }
+            //     }
+
+            //     const body = JSON.stringify(newUser);
+
+            //     const res = await axios.post('/api/users', body, config);
+            //     console.log(res.data);
+            // } catch (err) {
+            //     console.error(err.respose.data);
+            // }
+        }
+    }
+
+    return (
+      <>
+        <h1 className="large text-primary">Sign Up</h1>
+        <p className="lead">
+          <i className="fas fa-user"></i> Create Your Account
+        </p>
+        <form className="form" onSubmit={e => onSubmit(e)}>
+          <div className="form-group">
+            <input
+              type="text"
+              placeholder="Name"
+              name="name"
+              value={name}
+              onChange={(e) => onChange(e)}
+              required
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="email"
+              placeholder="Email Address"
+              name="email"
+              required
+              value={email}
+              onChange={(e) => onChange(e)}
+            />
+            <small className="form-text">
+              This site uses Gravatar so if you want a profile image, use a
+              Gravatar email
+            </small>
+          </div>
+          <div className="form-group">
+            <input
+              type="password"
+              placeholder="Password"
+              name="password"
+              minLength="6"
+              value={password}
+              onChange={(e) => onChange(e)}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="password"
+              placeholder="Confirm Password"
+              name="password2"
+              minLength="6"
+              value={password2}
+              onChange={(e) => onChange(e)}
+            />
+          </div>
+          <input type="submit" className="btn btn-primary" value="Register" />
+        </form>
+        <p className="my-1">
+          Already have an account? <Link to="/login">Sign In</Link>
+        </p>
+      </>
+    );
+}
+
+Register.propTypes = {
+  setAlert: PropTypes.func.isRequired
+};
+
+export default connect(null, {setAlert})(Register);
